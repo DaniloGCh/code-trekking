46d5a511da775664e4646ee0d788ddb7d58e2259
Ismael Horta <is.horta@duocuc.cl>
2026-08-20 17:18:49 -0400

    cambios capacitor andreoidmanifest weather-global

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,13 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'code-trekking',
-  webDir: 'www'
+  webDir: 'www',
+  server: {
+    androidScheme: 'https',
+    cleartext: true
+  }
 };
 
 export default config;
