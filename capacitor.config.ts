d454fb56f13c4ffa6ae25c4b7e6d54153a3d4d1e
Ismael Horta <is.horta@duocuc.cl>
2026-08-20 01:19:17 -0400

    Fuerza a Capacitor a interceptar las peticiones HttpClient y realizarlas nativamente sin restricciones de CORS/WebView (Prueba test para ver clima)

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'code-trekking',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    CapacitorHttp: {
+      enabled: true, // ✅ Fuerza a Capacitor a interceptar las peticiones HttpClient y realizarlas nativamente sin restricciones de CORS/WebView
+    },
+  },
 };
 
 export default config;
