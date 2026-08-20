77c95c10f501e8e413931d5e3fb36e5642481231
Ismael Horta <is.horta@duocuc.cl>
2026-08-20 01:47:14 -0400

    volviendo a lo del Claude

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,14 +1,9 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'code-trekking',
-  webDir: 'www',
-  plugins: {
-    CapacitorHttp: {
-      enabled: true, // ✅ Fuerza a Capacitor a interceptar las peticiones HttpClient y realizarlas nativamente sin restricciones de CORS/WebView
-    },
-  },
+  webDir: 'www'
 };
 
 export default config;
