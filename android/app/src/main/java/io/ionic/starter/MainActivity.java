d4a48312e6239000835612b6dca324c717265af4
Ismael Horta <is.horta@duocuc.cl>
2026-09-03 22:54:33 -0400

    ACtualizacion de MainActivity.java

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,25 +1,11 @@
 package io.ionic.starter;
 
 import android.os.Bundle;
-import android.webkit.WebView;
-import android.webkit.WebSettings;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Habilitar popups y JavaScript para pasarelas de pago como PayPal
-        WebView webView = this.getBridge().getWebView();
-        if (webView != null) {
-            WebSettings webSettings = webView.getSettings();
-            webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
-            webSettings.setMultipleWindows(true);
-        }
     }
 }
